661dbc338a5d868f48392f73394123e685011e50
mondirhallouli <halloulim@gmail.com>
2023-09-26 18:54:22 +0100

    added a few options and keymaps

--- lua/onepakman/set.lua
+++ lua/onepakman/set.lua
@@ -1,30 +1,40 @@
-
+-- line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- tabs and indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
+vim.opt.wrap = true
 
-vim.opt.wrap = false
-
+-- editing and buffer
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+vim.opt.cursorline = true
 
+-- search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true -- ignore case when searching
+vim.opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
+-- appearance
 vim.opt.termguicolors = true
+vim.opt.background = "dark"
 
+-- misc
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "90"
+
+-- clipboard
+vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register
